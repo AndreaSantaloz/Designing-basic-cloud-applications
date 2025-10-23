f6374894cbbb00b6933a0c1e989f8926fdbf277a
AndreaSantaloz <andreasantaloz@gmail.com>
2025-10-23 13:47:49 +0100

    A lot of documentation

--- app/Model/Employee.py
+++ app/Model/Employee.py
@@ -1,7 +1,2 @@
 
-
-class Employee:
-    def __init__(self,id,name,kind_job):
-        self.id=id
-        self.name= name
-        self.kind_job = kind_job
+print("He subido mi primera aplicación dockerizada")
